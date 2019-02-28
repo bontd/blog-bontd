c0b361f07e5aaba231291a2150a67a83eec13a40
Bon td <bontd@beetsoft.com.vn>
2019-02-28 13:40:49 +0700

    update

--- wp-content/themes/template/single.php
+++ wp-content/themes/template/single.php
@@ -68,13 +68,15 @@
 					<div class="post-author">
 						<div class="media">
 							<div class="media-left">
-								<img class="media-object" src="<?php echo esc_url(home_url('/'));?>/wp-content/themes/template/assets/img/author.png" alt="">
+								<div class="media-object-figure">
+									<?php echo get_avatar( get_the_author_meta('user_email')); ?>
+								</div>
 							</div>
 							<div class="media-body">
 								<div class="media-heading">
-									<h3>John Doe</h3>
+									<h3><?php the_author(); ?></h3>
 								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+								<p><?php the_author_description(); ?></p>
 								<ul class="author-social">
 									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
 									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -94,93 +96,10 @@
 					</div>
 
 					<div class="post-comments">
-						<!-- comment -->
-						<div class="media">
-							<div class="media-left">
-								<img class="media-object" src="<?php echo esc_url(home_url('/'));?>/wp-content/themes/template/assets/img/avatar.png" alt="">
-							</div>
-							<div class="media-body">
-								<div class="media-heading">
-									<h4>John Doe</h4>
-									<span class="time">March 27, 2018 at 8:00 am</span>
-									<a href="#" class="reply">Reply</a>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-								<!-- comment -->
-								<div class="media">
-									<div class="media-left">
-										<img class="media-object" src="<?php echo esc_url(home_url('/'));?>/wp-content/themes/template/assets/img/avatar.png" alt="">
-									</div>
-									<div class="media-body">
-										<div class="media-heading">
-											<h4>John Doe</h4>
-											<span class="time">March 27, 2018 at 8:00 am</span>
-											<a href="#" class="reply">Reply</a>
-										</div>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-									</div>
-								</div>
-								<!-- /comment -->
-							</div>
-						</div>
-						<!-- /comment -->
-
-						<!-- comment -->
-						<div class="media">
-							<div class="media-left">
-								<img class="media-object" src="<?php echo esc_url(home_url('/'));?>/wp-content/themes/template/assets/img/avatar.png" alt="">
-							</div>
-							<div class="media-body">
-								<div class="media-heading">
-									<h4>John Doe</h4>
-									<span class="time">March 27, 2018 at 8:00 am</span>
-									<a href="#" class="reply">Reply</a>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-							</div>
-						</div>
-						<!-- /comment -->
+						
 					</div>
 				</div>
 				<!-- /comments -->
-
-				<!-- reply -->
-				<div class="section-row">
-					<div class="section-title">
-						<h2>Leave a reply</h2>
-						<p>your email address will not be published. required fields are marked *</p>
-					</div>
-					<form class="post-reply">
-						<div class="row">
-							<div class="col-md-4">
-								<div class="form-group">
-									<span>Name *</span>
-									<input class="input" type="text" name="name">
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<span>Email *</span>
-									<input class="input" type="email" name="email">
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<span>Website</span>
-									<input class="input" type="text" name="website">
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="form-group">
-									<textarea class="input" name="message" placeholder="Message"></textarea>
-								</div>
-								<button class="primary-button">Submit</button>
-							</div>
-						</div>
-					</form>
-				</div>
-				<!-- /reply -->
 			</div>
 			<!-- /Post content -->
 
